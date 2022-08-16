5103e6f0a6ac3e2fb64e2e28798e594917b44721
Samiksha Rodke <104275432+SamikshaRodke@users.noreply.github.com>
2022-08-16 19:51:57 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const customFileInput=document.querySelector("#custom_FileInput");
+        const custombtn=document.querySelector("#custom_btn");
+        function activecustomFileInput(){
+            customFileInput.click();
+}
